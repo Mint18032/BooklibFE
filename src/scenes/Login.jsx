07b84f3a71c82921cc197ce8af62dfd61a996bd7
duyanh0208 <21020263@vnu.edu.vn>
2023-05-06 17:58:23 +0700

    update sign in, sign up

--- src/scenes/Login.jsx
+++ src/scenes/Login.jsx
@@ -7,148 +7,202 @@ import { eyeOff } from 'react-icons-kit/feather/eyeOff';
 import { eye } from 'react-icons-kit/feather/eye';
 import Cookies from 'universal-cookie';
 
-function Login() {
-    const [username, setUsername] = useState();
-    const [password, setPassword] = useState();
-    const cookies = new Cookies();
+// function Login() {
+//     const [username, setUsername] = useState();
+//     const [password, setPassword] = useState();
+//     const cookies = new Cookies();
   
-    const handleSubmit = (e) => {
-      // Prevent the default submit and page reload
-      e.preventDefault();
+//     const handleSubmit = (e) => {
+//       // Prevent the default submit and page reload
+//       e.preventDefault();
   
-      // Handle validations
-      axios
-        .post("http://localhost:5000/login", { username, password })
-        .then((res) => {
-          console.log(res);
-          // setCookie('state', res.data.state, { path: '/' })
-          cookies.set("state", res.data.user, { path: "/" });
-          // cookies.get('myCat')
+//       // Handle validations
+//       axios
+//         .post("http://localhost:5000/login", { username, password })
+//         .then((res) => {
+//           console.log(res);
+//           // setCookie('state', res.data.state, { path: '/' })
+//           cookies.set("state", res.data.user, { path: "/" });
+//           // cookies.get('myCat')
   
-          localStorage.setItem("state", res.data.user);
-          window.location.assign(res.data.url);
-          window.dispatchEvent(new Event("loggedin"));
-        })
-        .catch((err) => console.log(err));
-      return () => {};
-    };
+//           localStorage.setItem("state", res.data.user);
+//           window.location.assign(res.data.url);
+//           window.dispatchEvent(new Event("loggedin"));
+//         })
+//         .catch((err) => console.log(err));
+//       return () => {};
+//     };
   
     
-    return (
-      <div>
-        <form action="" id="login" method="post" onSubmit={handleSubmit}>
-          <h1>Login</h1>
-          <p className="item">
-            <label for="Username"> Username </label>
-            <input
-              type="username"
-              name="username"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </p>
-          <p className="item">
-            <label for="password"> Password </label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </p>
-          <p className="item">
-            <input type="submit" value="Login" />
-          </p>
-        </form>
-      </div>
-    );
-  }
-  
-  export default Login;
-
-// function Login() {
-//     // let nav = useNavigate()
-//     // const [cookies, setCookie] = useCookies('state')
-//     const [type, setType] = useState('password');
-//     const [icon, setIcon] = useState(eyeOff);
-//     const cookies = new Cookies();
-
-//     // useEffect(() => {
-//     //     axios.get(`http://localhost:5000/login`, {
-//     //         headers: {
-//     //             "Access-Control-Allow-Headers": "Content-Type",
-//     //         },
-//     //     })
-//     //         .then((res) => {
-//     //             consoyle.log(res)
-//     //             // setCookie('state', res.data.state, { path: '/' })
-
-//     //             cookies.set('state', res.data.state, { path: '/' });
-//     //             // cookies.get('myCat')
-
-//     //             localStorage.setItem('state', (res.data.state));
-//     //             window.location.assign(res.data.auth_url)
-//     //             window.dispatchEvent(new Event("loggedin"));
-//     //         })
-//     //         .catch((err) => console.log(err));
-//     //     return () => {
-//     //     }
-//     // }, []);
-//     const handleToggle = () => {
-//         if (type === 'password') {
-//             setIcon(eye);
-//             setType('text');
-//         }
-//         else {
-//             setIcon(eyeOff);
-//             setType('password');
-//         }
-//     }
 //     return (
-//         <>
-//         <div className="container">
-//             <form action="" method="POST" autoComplete="on">
-//                 <h1 className="title has-text-grey">Đăng nhập</h1>
-//                 <div className="box">
-//                     <div className="field">
-//                     <input
-//                         type="email"
-//                         className="item input"
-//                         name="email"
-//                         placeholder="Email"
-//                         required=""
-//                         defaultValue=""
-//                     />
-//                     <div className="field">
-//                     <input
-//                         type={type}
-//                         id="password"
-//                         className="item input"
-//                         name="password"
-//                         placeholder="Mật khẩu"
-//                         required=""
-//                         defaultValue=""
-//                     />
-//                     <span onClick={handleToggle}><Icon icon={icon} size={20}/></span>
-//                     </div>
-//                     <button type="submit" className="item button is-signin">
-//                     Đăng nhập
-//                     </button>
-//                     <div className="is-divider"></div>
-//                     <button className="item button is-signupGG">
-//                     <i className="fa-brands fa-google" />
-//                     Đăng nhập Google
-//                     </button>
-//                 </div>
-//                 <p className="has-text-grey has-text-right">
-//                     <a href="./register.html">Đăng ký</a>
-//                 </p>
-//                 </div>
-//             </form>
-//         </div>
-//         </>
+//       <div>
+//         <form action="" id="login" method="post" onSubmit={handleSubmit}>
+//           <h1>Login</h1>
+//           <p className="field">
+//             <label for="Username"> Username </label>
+//             <input
+//               type="username"
+//               name="username"
+//               id="username"
+//               value={username}
+//               onChange={(e) => setUsername(e.target.value)}
+//             />
+//           </p>
+//           <p className="item">
+//             <label for="password"> Password </label>
+//             <input
+//               type="password"
+//               name="password"
+//               id="password"
+//               value={password}
+//               onChange={(e) => setPassword(e.target.value)}
+//             />
+//           </p>
+//           <p className="item">
+//             <input type="submit" value="Login" />
+//           </p>
+//         </form>
+//       </div>
 //     );
-// }
-// export default Login
+//   }
+  
+//   export default Login;
+
+function Login() {
+    // let nav = useNavigate()
+    // const [cookies, setCookie] = useCookies('state')
+    // const [username, setUsername] = useState();
+    // const [type, setType] = useState('password');
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+    const [type, setType] = useState('password');
+    const [icon, setIcon] = useState(eyeOff);
+    const cookies = new Cookies();
+
+    const handleSubmit = (e) => {
+            // Prevent the default submit and page reload
+            e.preventDefault();
+        
+            // Handle validations
+            axios
+              .post("http://localhost:5000/login", { username, password })
+              .then((res) => {
+                console.log(res);
+                // setCookie('state', res.data.state, { path: '/' })
+                cookies.set("state", res.data.user, { path: "/" });
+                // cookies.get('myCat')
+        
+                localStorage.setItem("state", res.data.user);
+                window.location.assign(res.data.url);
+                window.dispatchEvent(new Event("loggedin"));
+              })
+              .catch((err) => console.log(err));
+            return () => {};
+    };
+
+    const ggSubmitAPI = (e) => {
+      e.preventDefault();
+        axios.get(`http://localhost:5000/loginAPI`, {
+            headers: {
+                "Access-Control-Allow-Headers": "Content-Type",
+            },
+        })
+            .then((res) => {
+                console.log(res)
+                // setCookie('state', res.data.state, { path: '/' })
+
+                cookies.set('state', res.data.state, { path: '/' });
+                // cookies.get('myCat')
+
+                localStorage.setItem('state', (res.data.state));
+                window.location.assign(res.data.auth_url)
+                window.dispatchEvent(new Event("loggedin"));
+            })
+            .catch((err) => console.log(err));
+        return () => {
+        }
+    };
+
+    const CallBack = (e) => {
+      axios.get(`http://localhost:5000/callback`, {
+          headers: {
+              "Access-Control-Allow-Headers": "Content-Type",
+          },
+      })
+          .then((res) => {
+              console.log(res)
+              // setCookie('state', res.data.state, { path: '/' })
+
+              cookies.set('state', res.data.state, { path: '/' });
+              // cookies.get('myCat')
+
+              localStorage.setItem('state', (res.data.state));
+              window.location.assign(res.data.auth_url)
+              window.dispatchEvent(new Event("loggedin"));
+          })
+          .catch((err) => console.log(err));
+      return () => {
+      }
+  };
+
+    const handleToggle = () => {
+        if (type === 'password') {
+            setIcon(eye);
+            setType('text');
+        }
+        else {
+            setIcon(eyeOff);
+            setType('password');
+        }
+    }
+    return (
+        <>
+        <div className="container">
+            <form action="" method="POST" autoComplete="on" onSubmit={handleSubmit}>
+                <h1 className="title has-text-grey">Đăng nhập</h1>
+                <div className="box">
+                    <div className="field">
+                    <div className="field">
+                    <input
+                        type={username}
+                        id="username"
+                        className="item input"
+                        name="username"
+                        placeholder="Tài khoản"
+                        required=""
+                        defaultValue=""
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    </div>
+                    <div className="field">
+                    <input
+                        type={password}
+                        id="password"
+                        className="item input"
+                        name="password"
+                        placeholder="Mật khẩu"
+                        required=""
+                        defaultValue=""
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <span onClick={handleToggle}><Icon icon={icon} size={20}/></span>
+                    </div>
+                    <button type="submit" className="item button is-signin">
+                    Đăng nhập
+                    </button>
+                    <div className="is-divider"></div>
+                    <button className="item button is-signupGG" onClick={ggSubmitAPI}>
+                    <i className="fa-brands fa-google" />
+                    Đăng nhập Google
+                    </button>
+                </div>
+                <p className="has-text-grey has-text-right">
+                    <a href="./register">Đăng ký</a>
+                </p>
+                </div>
+            </form>
+        </div>
+        </>
+    );
+}
+export default Login
--- src/scenes/Register.jsx
+++ src/scenes/Register.jsx
@@ -1,16 +1,49 @@
 import React from 'react';
 import '../styles/general.css'
 import { useState } from 'react';
+import axios from "axios";
 import { Icon } from 'react-icons-kit';
 import {eyeOff} from 'react-icons-kit/feather/eyeOff'
 import {eye} from 'react-icons-kit/feather/eye'
 import { Helmet } from 'react-helmet';
+import Cookies from 'universal-cookie';
 
 
 function Register() {
 
-    const [type, setType]=useState('password');
-    const [icon, setIcon]=useState(eyeOff);
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+    const [email, setEmail] = useState();
+    const [type, setType] = useState('password');
+    const [icon, setIcon] = useState(eyeOff);
+    const cookies = new Cookies();
+
+    const handleRegister = (e) => {
+        // Prevent the default submit and page reload
+        e.preventDefault();
+    
+        // Handle validations
+        axios
+          .post("http://localhost:5000/register", {email, username, password})
+          .then((res) => {
+            console.log(res);
+            // setCookie('state', res.data.state, { path: '/' })
+            cookies.set("state", res.data.user, { path: "/" });
+            // cookies.get('myCat')
+    
+            localStorage.setItem("state", res.data.user);
+            Noti();
+            window.location.assign("http://localhost:3000/login");
+            window.dispatchEvent(new Event("loggedin"));
+
+          })
+          .catch((err) => console.log(err));
+        return () => {};
+    };
+
+    function Noti() {
+        alert("Đăng ký thành công");
+    }
 
     const handleToggle = () => {
         if(type==='password') {
@@ -30,7 +63,7 @@ function Register() {
         </Helmet>
         
         <div className="container">
-            <form action="" method="POST" autoComplete="on">
+            <form action="" method="POST" autoComplete="on" onSubmit={handleRegister}>
                 <div>
                 <h1 className="title has-text-grey">Đăng ký</h1>
                 <div className="box">
@@ -42,16 +75,18 @@ function Register() {
                         placeholder="Email"
                         required=""
                         defaultValue=""
+                        onChange={(e) => setEmail(e.target.value)}
                     />
                     </div>
                     <div className="field">
                     <input
                         type="text"
                         className="item input"
-                        name="name"
+                        name="username"
                         placeholder="Tên bạn"
                         required=""
                         defaultValue=""
+                        onChange={(e) => setUsername(e.target.value)}
                     />
                     </div>
                     <div className="field">
@@ -63,22 +98,17 @@ function Register() {
                         placeholder="Mật khẩu"
                         required=""
                         defaultValue=""
+                        onChange={(e) => setPassword(e.target.value)}
                     />
                     <span onClick={handleToggle}><Icon icon={icon} size={20}/></span>
                     </div>
-                    <button type="submit" className="item button is-signin">
+                    <button type="submit" className="item button is-signin" onClick={Noti}>
                     Đăng ký
                     </button>
-                    <div className="is-divider">
-                    <span>HOẶC</span>
-                    </div>
-                    <button className="item button is-signupGG">
-                    <i className="fa-brands fa-google" />
-                    Đăng nhập với Google
-                    </button>
+
                 </div>
                 <p className="has-text-grey has-text-right">
-                    <a href="./login.html">Đăng nhập</a>
+                    <a href="./login">Đăng nhập</a>
                 </p>
                 </div>
             </form>
